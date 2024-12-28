7976ee60e36a0747ad7e652cae1c0678c32e5c47
WaffleAWT <alinajjarwork@gmail.com>
2024-12-28 07:22:04 +0300

    added DialogueMessage on plugin initialization

--- simple_dialogue/simple_dialogue.gd
+++ simple_dialogue/simple_dialogue.gd
@@ -2,7 +2,9 @@
 extends EditorPlugin
 
 func _enter_tree() -> void:
+	add_custom_type("DialogueMessage", "Resource", preload("res://addons/simple_dialogue/Scripts/dialogue_message.gd"), preload("res://addons/simple_dialogue/Assets/Dialogue Message.png"))
 	add_custom_type("Dialogue", "Label", preload("res://addons/simple_dialogue/simple_dialogue.gd"), preload("res://addons/simple_dialogue/Assets/Dialogue.png"))
 
 func _exit_tree() -> void:
 	remove_custom_type("Dialogue")
+	remove_custom_type("DialogueMessage")
